24a7c979475d2bd8416f4e4ed2a12e46116fa936
Mark <aguilarmark19@gmail.com>
2017-07-29 22:54:21 -0500

    files

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<title>Resume </title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+
+    <!-- Left Column -->
+    <div class="w3-third">
+
+      <div class="w3-white w3-text-grey w3-card-4">
+        <!--
+        <div class="w3-display-container">
+          <img src="/w3images/avatar_hat.jpg" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-container w3-text-black">
+            <h2>Mark Aguilar</h2>
+          </div>
+        </div>
+      -->
+        <div class="w3-container">
+          <p><i class="fa fa-user circle fa-fw w3-margin-right w3-large w3-text-teal"></i>Mark A. Aguilar</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Houston, TX </p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large w3-text-teal"></i>aguilarmark19@gmail.com</p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>832-229-0061</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right w3-text-teal"></i>Skills</b></p>
+          <p>Web Devlopment: <br> Boostrap, HTML, Javascript, SQL, MySQL, PHP, phpmyadmin</p>
+          <p>Programs:<br> Microsoft Office 365, LaTex, Kleopatra, Nmap, VMware Workstation Player, Wireshark</p>
+          <p>Operating Systems:<br> Kali Linux, Mac, Ubuntu, Windows</p>
+
+
+<hr>
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Languages</b></p>
+          <p>English</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:100%"></div>
+          </div>
+          <p>Spanish</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-round-xlarge w3-teal" style="height:24px;width:25%"></div>
+          </div>
+          <!--
+          <p></p>
+          <div class="w3-white w3-round-xlarge">
+            <div class="w3-round-xlarge w3-white" style="height:24px;width:25%"></div>
+          </div>
+        -->
+          <br>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+
+      <div class="w3-container w3-card-2 w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Work Experience</h2>
+        <div class="w3-container">
+         <h5 class="w3-opacity"><b>Server / P.F. Changs</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>June 2016 - <span class="w3-tag w3-teal w3-round">Current</span></h6>
+          <p>Devliver friendly and effieicent service to ensure a pleasant dining experience for guest<br>
+          Memorize details for food and drink menuss, happy hour, and pormotions<br>
+          Polish plates and stock galsses to make sure the restaurant continues to function properly</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Guest Services Representative / Houston Astros</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>May 2015 - Aug 2016</h6>
+          <p>Prepared seating sections for patrons and assisted fans in locating seats<br>
+          Scanned game tickets insuring that the proper ticket is being used to gain entry<br>
+          Oversaw assigned area for potential hazards in the ballpark and reported incidents</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Key Holder / RadioShack</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Nov 2013 - Aug 2014</h6>
+          <p>Controlled opening / closing the store and managed finances<br>
+          Sold approximately $10,000 worth of merchandise and 300 tickets monthly<br>
+          Used communication skills to help customers find certain products</p>
+          <br>
+        </div>
+      </div>
+
+      <div class="w3-container w3-card-2 w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Education</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>University of Houston</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2015 - Current</h6>
+          <ul><li>Bachelor of Science in Computer Information Systems</li></ul>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Lone Star College</b></h5>
+          <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2012 - 2014</h6>
+          <ul><li>Associates of Science</li></ul>
+          <hr>
+        </div>
+
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+
+  <!-- End Grid -->
+  </div>
+
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-teal w3-center w3-margin-top">
+<br>
+  <!--<p>Find me on social media.</p>
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i> -->
+  <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>
+
+</body>
+</html>
